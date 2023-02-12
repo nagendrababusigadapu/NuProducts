f12f9a92f8475eeb5c392309fda5cf63b4147237
Nagendra Babu <nagendrababu.iosdeveloper@gmail.com>
2023-02-12 20:44:27 +0530

    Created model file

---
+++ NuProducts/Models/ProductsList.swift
@@ -0,0 +1,26 @@
+//
+//  ProductsList.swift
+//  NuProducts
+//
+//  Created by Nagendra Babu on 12/02/23.
+//
+
+import Foundation
+
+
+struct ProductList: Codable {
+    let products: [Product]
+    let total, skip, limit: Int
+}
+
+// MARK: - Product
+struct Product: Codable {
+    let id: Int
+    let title, description: String
+    let price: Int
+    let discountPercentage, rating: Double
+    let stock: Int
+    let brand, category: String
+    let thumbnail: String
+    let images: [String]
+}
